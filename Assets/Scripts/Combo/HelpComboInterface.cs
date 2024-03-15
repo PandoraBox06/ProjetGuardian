975e681f7a76454ee4d69a98fb25acfaa283bc46
Blanco <96238298+BlancoTLM@users.noreply.github.com>
2024-03-15 15:54:04 +0100

    small change in code logic

--- Assets/Scripts/Combo/HelpComboInterface.cs
+++ Assets/Scripts/Combo/HelpComboInterface.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class HelpComboInterface : MonoBehaviour
 {
+    public static HelpComboInterface Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+    
     [SerializeField] private TextMeshProUGUI comboText;
     [SerializeField] private TextMeshProUGUI scoreText;
     [SerializeField] private TextMeshProUGUI lastComboText;
-
     private int score;
 
     private void Start()
     {
-        // BlancoCombatManager.Instance.Whathever.AddListener(AddScore);
         BlancoCombatManager.Instance.InputEvent.AddListener(AddCombo);
         BlancoCombatManager.Instance.CancelEvent.AddListener(CleanCombo);
         BlancoCombatManager.Instance.FinishedComboEvent.AddListener(FinishedCombo);
@@ -35,7 +45,7 @@ public class HelpComboInterface : MonoBehaviour
         }
     }
 
-    private void AddScore(int _score)
+    public void AddScore(int _score)
     {
         score += _score;
         scoreText.text = $"Score : {score}";
@@ -49,5 +59,11 @@ public class HelpComboInterface : MonoBehaviour
     private void CleanCombo()
     {
         comboText.text = "";
+    }
+    
+    private void OnDestroy()
+    {
+        if (this == Instance)
+            Instance = null;
     }
 }
