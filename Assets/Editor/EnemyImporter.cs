64e0158a00bca4512464b1f5b4234cebf9b3ff61
PandoraBox06 <axel.heros06@hotmail.fr>
2024-03-15 19:43:21 +0100

    Edward Tool Fini

--- Assets/Editor/EnemyImporter.cs
+++ Assets/Editor/EnemyImporter.cs
@@ -1,6 +1,5 @@
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 
 public class EnemyImporter : EditorWindow
@@ -11,31 +10,35 @@ public class EnemyImporter : EditorWindow
     public static void ShowWindow()
     {
         var window = GetWindow<EnemyImporter>();
-        window.titleContent = new GUIContent("Enemy Importer");
+        window.titleContent = new GUIContent("Alphonse: Grand Frere...");
         window.Show();
     }
 
     private void OnGUI()
     {
-        EditorGUILayout.LabelField("Output");
-        outPut = EditorGUILayout.ObjectField(outPut, typeof(GameObject), false) as GameObject;
-        EditorGUILayout.LabelField("Origin");
+        EditorGUILayout.LabelField("From");
         origin = EditorGUILayout.ObjectField(origin, typeof(GameObject), false) as GameObject;
+        EditorGUILayout.LabelField("To");
+        outPut = EditorGUILayout.ObjectField(outPut, typeof(GameObject), false) as GameObject;
         string assetPathOutput = AssetDatabase.GetAssetPath(outPut);
-        string assetPathOrigin = AssetDatabase.GetAssetPath(origin);
         
         if (GUILayout.Button("Unpack"))
         {
-            GameObject originRoot = PrefabUtility.LoadPrefabContents(assetPathOrigin);
+            var newOrigin = PrefabUtility.InstantiatePrefab(origin) as GameObject;
+            PrefabUtility.SaveAsPrefabAsset(newOrigin, "Assets/Prefab/newPrefab.prefab");
+            DestroyImmediate(newOrigin);
+            GameObject originRoot = PrefabUtility.LoadPrefabContents("Assets/Prefab/newPrefab.prefab");
             GameObject outputRoot = PrefabUtility.LoadPrefabContents(assetPathOutput);
             GameObject skin = originRoot.GetComponentInChildren<SkinnedMeshRenderer>().gameObject;
-
-            Destroy(outputRoot.GetComponentInChildren<SkinnedMeshRenderer>().gameObject);
+            var mask = originRoot.GetComponentInChildren<Animator>().avatar;
+            
+            DestroyImmediate(outputRoot.GetComponentInChildren<SkinnedMeshRenderer>().gameObject);
+            outputRoot.GetComponentInChildren<Animator>().avatar = mask;
             Instantiate(skin, outputRoot.transform, false);
             
-            
-            PrefabUtility.SaveAsPrefabAsset(outputRoot, assetPathOrigin);
+            PrefabUtility.SaveAsPrefabAsset(outputRoot, assetPathOutput);
             PrefabUtility.UnloadPrefabContents(outputRoot);
+            AssetDatabase.DeleteAsset("Assets/Prefab/newPrefab.prefab");
         }
     }
 }
