0b0bab6d40124dbe74ba972d89f587d8aa15233d
Blanco <96238298+BlancoTLM@users.noreply.github.com>
2024-05-15 18:47:21 +0200

    anim de barre de vie

--- Assets/Scripts/Player/PlayerHpBar.cs
+++ Assets/Scripts/Player/PlayerHpBar.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerHpBar : MonoBehaviour
 {
     public Slider slider;
+    [SerializeField] private RectTransform fill;
+    [SerializeField] private RectTransform animFill;
 
     private void OnEnable()
     {
@@ -20,10 +23,12 @@ public class PlayerHpBar : MonoBehaviour
     {
         slider.maxValue = PlayerData.Instance.maxHealth;
         slider.value = PlayerData.Instance.currentHealth;
+        animFill.anchorMax = fill.anchorMax;
     }
 
     void UpdateHP(float hp)
     {
         slider.value = hp;
+        animFill.DOAnchorMax(fill.anchorMax, 1f);
     }
 }
