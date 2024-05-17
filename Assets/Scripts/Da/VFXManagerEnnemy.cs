48740213c7510fe3985a76a661b7ea7f6d2bec6a
PandoraBox06 <axel.heros06@hotmail.fr>
2024-05-17 17:04:01 +0200

    New Anim

--- Assets/Scripts/Da/VFXManagerEnnemy.cs
+++ Assets/Scripts/Da/VFXManagerEnnemy.cs
@@ -8,17 +8,13 @@ public class VFXManagerEnnemy : MonoBehaviour
     public ParticleSystem SlashEnnemy2;
     public ParticleSystem SlashEnnemy3;
     public ParticleSystem StunEnnemy;
-    public ParticleSystem InstanceAttaque;
 
     void SlashEnemy()
     {
         SlashEnnemy1.Play();
         SlashEnnemy2.Play();
     }
-    void InstanceAttaqueEnemy()
-    {
-        InstanceAttaque.Play();
-    }
+
     void SlashEnemy2()
     {
         SlashEnnemy3.Play();
